b1fee8fc1fdf51e38f3d8290933335afa7071384
ftxj <932141413@qq.com>
2023-04-29 16:16:11 +0800

    fix typp

--- deploy.cmd
+++ deploy.cmd
@@ -2,3 +2,4 @@ git pull
 cd ..
 hexo g
 hexo d
+cd source
--- jupyter/toBlog.py
+++ jupyter/toBlog.py
@@ -7,19 +7,21 @@ parser = argparse.ArgumentParser(description="toBlog")
 parser.add_argument('--path', required=True, type=str)
 args = parser.parse_args()
 
+
 def get_file_name(path):
     filename = str(os.path.split(path)[1])
     x = os.path.splitext(filename)[0]
     return x
+
 
 def time_string():
     time_now = datetime.now()
     s = time_now.strftime("%Y-%m-%d %H:%M:%S")
     return s
 
+
 def header():
     head = '''
-<!-- toBlog..runed -->
 ---
 layout:
   - post
@@ -31,21 +33,25 @@ tags: {2}
     print(head)
     return head
 
+
 def write_meta_info():
     with open(args.path, "r+") as f:
         old = f.read()
         lines = old.splitlines()
-        if "<!-- toBlog..runed -->" == lines[0]:
+        if "---" == lines[0]:
             return
         f.seek(0)
         f.write(header())
         f.write(old)
 
+
 def post_this_md():
     original_path = args.path
     new_path = "../_posts/" + get_file_name(args.path) + ".md"
     os.system("cp {0} {1}".format(original_path, new_path))
-    os.system("cd ..; git add .; git commit -m 'add {0}'; git push".format(get_file_name))
+    os.system(
+        "cd ..; git add .; git commit -m 'add {0}'; git push".format(get_file_name))
+
 
 write_meta_info()
 post_this_md()
